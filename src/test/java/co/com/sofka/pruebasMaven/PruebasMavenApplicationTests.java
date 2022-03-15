ab6f1354d56abaf07d29f7079d8c825904184619
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-14 20:29:46 -0500

    se crea proyecto con spring boot

---
+++ src/test/java/co/com/sofka/pruebasMaven/PruebasMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.pruebasMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
