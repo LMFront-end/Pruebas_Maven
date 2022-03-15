445296e7526d27839fa4bf2b66649befb5ac3ab9
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-14 20:39:27 -0500

    se crea metodo subt

--- src/main/java/co/com/sofka/pruebasMaven/calculator/BasicCalculator.java
+++ src/main/java/co/com/sofka/pruebasMaven/calculator/BasicCalculator.java
@@ -7,8 +7,23 @@ public class BasicCalculator {
 
     private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
 
+    // suma
     public Long sum(Long number1, Long number2) {
-        logger.info( "Summing {} + {}", number1, number2 );
+        logger.info( "Adding {} + {}", number1, number2 );
         return number1 + number2;
     }
+
+    // resta
+    public Long subt(Long number1, Long number2) {
+        logger.info( "Subtraction {} - {}", number1, number2 );
+        return number1 - number2;
+    }
+
+    // multiplicación
+
+
+    // división
+
+
+    // modulo
 }
