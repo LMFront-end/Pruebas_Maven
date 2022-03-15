44e977aa9306ab908d86f21b1de05d6e7a6e8b28
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-14 20:35:19 -0500

    se importa Logger y LoggerFactory

---
+++ src/main/java/co/com/sofka/pruebasMaven/calculator/BasicCalculator.java
@@ -0,0 +1,11 @@
+package co.com.sofka.pruebasMaven.calculator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasicCalculator {
+
+    private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
+
+    
+}
