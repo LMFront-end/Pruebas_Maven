df57f3fea700b81220740d5f5d0c86964928d327
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-14 21:04:28 -0500

    se crea archivo test

---
+++ src/test/java/co/com/sofka/pruebasMaven/calculator/BasicCalculatorTest.java
@@ -0,0 +1,31 @@
+package co.com.sofka.pruebasMaven.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasicCalculatorTest {
+
+    // Objects instances
+    private final BasicCalculator basicCalculator = new BasicCalculator();
+
+    @Test
+    void sum() {
+    }
+
+    @Test
+    void subt() {
+    }
+
+    @Test
+    void mult() {
+    }
+
+    @Test
+    void div() {
+    }
+
+    @Test
+    void mod() {
+    }
+}
