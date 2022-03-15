807db4029cc57a219f27c5d631c01d1caa28469f
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-14 21:55:48 -0500

    se hace test utilizando csv en subt

--- src/test/java/co/com/sofka/pruebasMaven/calculator/BasicCalculatorCsvTest.java
+++ src/test/java/co/com/sofka/pruebasMaven/calculator/BasicCalculatorCsvTest.java
@@ -11,6 +11,7 @@ class BasicCalculatorCsvTest {
 
     BasicCalculator basicCalculator = new BasicCalculator();
 
+    // sum
     @DisplayName("Testing several sums")
     @ParameterizedTest(name = "{0} + {1} = {2}")
     @CsvSource({
@@ -19,9 +20,23 @@ class BasicCalculatorCsvTest {
             "49,  51, 100",
             "1,  100, 101"
     })
-
     public void severalSums(Long first, Long second, Long expectedResult) {
         assertEquals(expectedResult, basicCalculator.sum(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+    }
+
+    // sub
+
+    @DisplayName("Testing several subts")
+    @ParameterizedTest(name = "{0} - {1} = {2}")
+    @CsvSource({
+            "2,    1,   1",
+            "5,    2,   3",
+            "49,  51, -2",
+            "1,  100, -99"
+    })
+    public void severalSubts(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.subt(first, second),
                 () -> first + " + " + second + " should equal " + expectedResult);
     }
 
