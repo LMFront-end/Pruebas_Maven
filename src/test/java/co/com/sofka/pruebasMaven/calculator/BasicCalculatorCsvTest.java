0473ba3b2a45987b11923890a1fe890b41cb5005
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-14 21:51:54 -0500

    se hace test utilizando csv en sum

---
+++ src/test/java/co/com/sofka/pruebasMaven/calculator/BasicCalculatorCsvTest.java
@@ -0,0 +1,32 @@
+package co.com.sofka.pruebasMaven.calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasicCalculatorCsvTest {
+
+    BasicCalculator basicCalculator = new BasicCalculator();
+
+    @DisplayName("Testing several sums")
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,    1,   1",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+
+    public void severalSums(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.sum(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+    }
+
+
+
+
+
+}
